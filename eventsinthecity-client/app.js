b0c290b18b324e56306d5ce84b3bc0c62398a330
anushkajain11 <anushka.jain1499@gmail.com>
2017-10-29 23:09:44 -0700

    redirection to login page

--- eventsinthecity-client/app.js
+++ eventsinthecity-client/app.js
@@ -41,6 +41,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/clickOnLoginButton', routes.clickOnLoginButton);
 app.get('/users', user.list);
 app.post('/login', login.login);
 app.post('/register',login.register);
